1f14bf049520ac08a897dde42ff1fea06e124a33
Kush <kushmaniar@gmail.com>
2024-01-07 18:25:33 +0530

    sidebar ui change

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import MuiDrawer from '@mui/material/Drawer'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import {
   AppBar,
   Avatar,
@@ -13,103 +13,102 @@ import {
   CardMedia,
   Toolbar,
   Tooltip,
-} from '@mui/material'
-import { Icon } from '@iconify/react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router'
-import { btn, circularImage, df_jc_ac, df_jfe_ac } from '../../theme/CssMy'
-import { useContext } from 'react'
-import { eycontext } from '../../context/MainContext'
-import { generateFromString } from 'generate-avatar'
-import slogo from '../../images/logo.png'
+} from "@mui/material";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { btn, circularImage, df_jc_ac, df_jfe_ac } from "../../theme/CssMy";
+import { useContext } from "react";
+import { eycontext } from "../../context/MainContext";
+import { generateFromString } from "generate-avatar";
+import slogo from "../../images/logo-alt.png";
 
-const drawerWidth = 240
+const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
-})
+  overflowX: "hidden",
+});
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(8)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
-})
+});
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
-  boxShadow: '0px 1px 26px rgba(94, 99, 116, 0.05)',
-  whiteSpace: 'nowrap', 
-  boxSizing: 'border-box',
+  boxShadow: "0px 1px 26px rgba(94, 99, 116, 0.05)",
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
   ...(open && {
     ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
   ...(!open && {
     ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
   }),
-}))
+}));
 
 const imgStyle = {
-  borderRadius: '50px',
-}
+  borderRadius: "50px",
+};
 
 const listItemBtn = {
-  justifyContent: 'initial',
+  justifyContent: "initial",
   px: 2.5,
-}
+};
 
 const listItemIco = {
   minWidth: 0,
-  justifyContent: 'center',
-}
+  justifyContent: "center",
+};
 
 const gridItem = {
-  padding: '8px',
-  minHeight: '20px',
-}
+  padding: "8px",
+  minHeight: "20px",
+};
 
 const gridcon = {
-  display: 'flex',
-  alignItems: 'space-between',
-  flexDirection: 'column',
-  height: '100vh',
-  justifyContent: 'space-between',    
-}  
- 
+  display: "flex",
+  alignItems: "space-between",
+  flexDirection: "column",
+  height: "100vh",
+  justifyContent: "space-between",
+};
+
 export default function SideDrawer(props) {
-  const { children } = props
-  const url = window.location.href.split('/')[3]
-  const { user, setUser, setToken } = useContext(eycontext)
-  const navigate = useNavigate()
+  const { children } = props;
+  const url = window.location.href.split("/")[3];
+  const { user, setUser, setToken } = useContext(eycontext);
+  const navigate = useNavigate();
   // console.log(url)
   // console.log(user)
-  const isDoc = user ? (user.role === 'doctor' ? true : false) : ('')
+  const isDoc = user ? (user.role === "doctor" ? true : false) : "";
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <AppBar
         sx={{
-          marginLeft: '50px',
-          backgroundColor: 'white',
-          color: '#005739',
-          boxShadow: '0px 1px 26px rgba(94, 99, 116, 0.05)',
-        }}
-      >
+          marginLeft: "50px",
+          backgroundColor: "white",
+          color: "#005739",
+          boxShadow: "0px 1px 26px rgba(94, 99, 116, 0.05)",
+        }}>
         <Toolbar sx={df_jfe_ac}>
           {/* <Avatar
             sx={{ marginRight: '2%' }}
@@ -119,22 +118,21 @@ export default function SideDrawer(props) {
           <Button
             sx={btn}
             onClick={() => {
-              localStorage.setItem('eyUser', null)
-              localStorage.setItem('eyToken', null)
-              navigate('/login')
-            }}
-          >
+              localStorage.setItem("eyUser", null);
+              localStorage.setItem("eyToken", null);
+              navigate("/login");
+            }}>
             Logout
           </Button>
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent">
         <Box sx={gridcon}>
-          <Box sx={{ ...df_jc_ac, marginTop: '5%' }}>
+          <Box sx={{ ...df_jc_ac, marginTop: "5%" }}>
             <CardMedia
               component="img"
               image={slogo}
-              sx={{ borderRadius: '50px', width: '70%', marginTop: '15%' }}
+              sx={{ borderRadius: "50px", width: "70%", marginTop: "15%" }}
             />
           </Box>
           <Box>
@@ -143,14 +141,13 @@ export default function SideDrawer(props) {
                 <Tooltip title="Dashboard">
                   <ListItem
                     disablePadding
-                    onClick={() => navigate('/')}
-                    sx={{ display: 'block', marginTop: '20%' }}
-                  >
+                    onClick={() => navigate("/")}
+                    sx={{ display: "block", marginTop: "20%" }}>
                     <ListItemButton sx={listItemBtn}>
                       <ListItemIcon sx={listItemIco}>
                         <Icon
                           icon="material-symbols:home-rounded"
-                          color={url === '' ? '#005739' : '#6A707F'}
+                          color={url === "" ? "#fff" : "#6A707F"}
                           width="26"
                           height="26"
                         />
@@ -161,14 +158,13 @@ export default function SideDrawer(props) {
                 <Tooltip title="Patients">
                   <ListItem
                     disablePadding
-                    onClick={() => navigate('/allpatients')}
-                    sx={{ display: 'block', marginTop: '20%' }}
-                  >
+                    onClick={() => navigate("/allpatients")}
+                    sx={{ display: "block", marginTop: "20%" }}>
                     <ListItemButton sx={listItemBtn}>
                       <ListItemIcon sx={listItemIco}>
                         <Icon
                           color={
-                            url.includes('allpatients') ? '#005739' : '#6A707F'
+                            url.includes("allpatients") ? "#fff" : "#6A707F"
                           }
                           icon="material-symbols:patient-list"
                           width="30"
@@ -181,14 +177,13 @@ export default function SideDrawer(props) {
                 <Tooltip title="Appointments">
                   <ListItem
                     disablePadding
-                    onClick={() => navigate('/appointments')}
-                    sx={{ display: 'block', marginTop: '20%' }}
-                  >
+                    onClick={() => navigate("/appointments")}
+                    sx={{ display: "block", marginTop: "20%" }}>
                     <ListItemButton sx={listItemBtn}>
                       <ListItemIcon sx={listItemIco}>
                         <Icon
                           color={
-                            url.includes('appointments') ? '#005739' : '#6A707F'
+                            url.includes("appointments") ? "#fff" : "#6A707F"
                           }
                           icon="teenyicons:appointments-solid"
                           width="24"
@@ -205,34 +200,15 @@ export default function SideDrawer(props) {
                   <Tooltip title="Dashboard">
                     <ListItem
                       disablePadding
-                      onClick={() => navigate('/')}
-                      sx={{ display: 'block', marginTop: '20%' }}
-                    >
+                      onClick={() => navigate("/")}
+                      sx={{ display: "block", marginTop: "20%" }}>
                       <ListItemButton sx={listItemBtn}>
                         <ListItemIcon sx={listItemIco}>
                           <Icon
                             icon="material-symbols:home-rounded"
-                            color={url === '' ? '#005739' : '#6A707F'}
+                            color={url === "" ? "#fff" : "#6A707F"}
                             width="26"
                             height="26"
-                          />
-                        </ListItemIcon>
-                      </ListItemButton>
-                    </ListItem>
-                  </Tooltip>
-                  <Tooltip title="Reports">
-                    <ListItem
-                      disablePadding
-                      onClick={() => navigate('/reports')}
-                      sx={{ display: 'block', marginTop: '20%' }}
-                    >
-                      <ListItemButton sx={listItemBtn}>
-                        <ListItemIcon sx={listItemIco}>    
-                          <Icon
-                            color={url.includes('reports') ? '#005739' : '#6A707F'}
-                            icon="fluent:notepad-28-filled"
-                            width="24"
-                            height="24"
                           />
                         </ListItemIcon>
                       </ListItemButton>
@@ -250,5 +226,5 @@ export default function SideDrawer(props) {
         {children}
       </Box>
     </Box>
-  )
+  );
 }
